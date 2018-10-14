2ba99866ad1cd2bad14de32530209e0c427c0da1
Charter <howtodieexactly@mail.ru>
2018-10-14 16:33:34 +0300

    Release

---
+++ SendExp/CommandEx.cs
@@ -0,0 +1,63 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendExp
+{
+    public class CommandEx : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "ex";
+
+        public string Help => "";
+
+        public string Syntax => "";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 2)
+            {
+                UnturnedChat.Say(caller, Plugin.Instance.Translate("invalid_args"));
+            }
+            else
+            {
+                UnturnedPlayer p = (UnturnedPlayer)caller;
+                UnturnedPlayer second = UnturnedPlayer.FromName(command[0]);
+                if (second == null)
+                {
+                    UnturnedChat.Say(caller, Plugin.Instance.Translate("not_found"));
+                }
+                else
+                {
+                    if (uint.TryParse(command[1], out var count))
+                    {
+                        if (p.Experience >= count)
+                        {
+                            p.Experience -= count;
+                            second.Experience += count;
+                            UnturnedChat.Say(p, Plugin.Instance.Translate("success_0", count, second.CharacterName ));
+                            UnturnedChat.Say(second, Plugin.Instance.Translate("success_1", p.CharacterName, count ));
+                        }
+                        else
+                        {
+                            UnturnedChat.Say(caller, Plugin.Instance.Translate("no_money"));
+                        }
+                    }
+                    else
+                    {
+                        UnturnedChat.Say(caller, Plugin.Instance.Translate("invalid_args"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SendExp/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendExp
+{
+    public class Plugin : RocketPlugin
+    {
+        public static Plugin Instance;
+        protected override void Load()
+        {
+            Instance = this;
+        }
+
+        protected override void Unload()
+        {
+            base.Unload();
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+            { "invalid_args", "Invalid args." },
+            { "success_0", "Вы отправили {0} опыта игроку {1}" },
+            { "success_1", "Игрок {0} вам отправил {1} опыта" },
+            { "not_found", "Игрок не найден" },
+            { "no_money", "У вас недостаточно опыта" }
+        };
+    }
+}
